628ec44b7e1010c51443719eb42d0324228aa20d
inredzero <caesete0524@naver.com>
2024-12-16 02:23:24 +0900

    공지사항 병합

--- src/main/java/com/lastdance/project/sessionsecurity/auth/controller/MainController.java
+++ src/main/java/com/lastdance/project/sessionsecurity/auth/controller/MainController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping(value = {"/", "/main"})
-    public String main() {
-        return "main";
-    }
+//    @GetMapping(value = {"/", "/main"})
+//    public String main() {
+//        return "main";
+//    }
 
     @GetMapping("/admin/page")
     public String adminPage() {
